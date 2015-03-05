bd2608565804eb817a42363da55cc2cd5e130592
Ivan <vankasieb@gmail.com>
2015-03-05 18:33:30 +0200

    123

---
+++ first_program.java
@@ -0,0 +1,15 @@
+package com.example.firstprogram;
+
+/**
+ * Created by Ivan on 05.03.2015.
+ */
+public class first_program {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World");
+        String[] sArray = {"1","2","3","4","5"};
+       for (int x=0; x<sArray.length;){
+            System.out.println(sArray[x++]);
+        }
+
+    }
+}
